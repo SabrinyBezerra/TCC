4702033b5035faf63e6177d5ff41c68b097b727c
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2018-08-22 19:20:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
-let acidente = undefined;
+let acidente = [];
 
-// Exemplo de requisição GET
+
+
+/*
+  // Exemplo de requisição GET
 let ajax = new XMLHttpRequest();
 
 // Seta tipo de requisição e URL com os parâmetros
@@ -15,14 +18,51 @@ ajax.onreadystatechange = function() {
 // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
     if (ajax.readyState == 4 && ajax.status == 200) {
         //console.log(ajax.responseText)
-        acidente = ajax.responseText;
+        acidente = ajax.response
+        console.log(acidente)
+        console.log('OK')
     }
 }
+
+*/
 
 //mostra os dados consultados na tela 
 function monstrarAcidente(){
     //window.open("pg2.html");
     //console.log(acidente);
-    document.getElementById('textAcidente').innerHTML = acidente;
+    console.log('OK')
+    $.ajax({
+        type:"GET",
+        url:"http://127.0.0.1:5000/api/acidente",
+        success: function(data) {
+          acidente = data;
+          console.log('SUCESSO')
+          criarGrafico()
+        },
+      });
+    //document.getElementById('textAcidente').innerHTML = acidente;
+}
+
+
+ function criarGrafico () {
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "Número de acidentes por UF"              
+		},
+		data: [              
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "column",
+			dataPoints: [
+				{ label: acidente[0][0],  y: acidente[0][1]  },
+				{ label: acidente[1][0], y: acidente[1][1]  },
+				{ label: acidente[2][0], y: acidente[2][1]  },
+				{ label: acidente[3][0],  y: acidente[3][1]  },
+				{ label: acidente[4][0],  y: acidente[4][1]  }
+			]
+		}
+		]
+	});
+	chart.render();
 }
 
