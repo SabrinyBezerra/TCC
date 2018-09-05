24d6803f96f47f67cf732065ec3ab32ec23a40b5
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2018-09-05 14:58:44 -0300

    Update systemController.js

--- TCC/TCC-Angular/js/controllers/systemController.js
+++ TCC/TCC-Angular/js/controllers/systemController.js
@@ -1,25 +1,28 @@
 app.controller('systemCtrl', function($scope, systemService){
     
-    $scope.title = "DEU CERTO OUUUUUUUUUUUU";
-    var acidente;
-
-    $scope.verifica = function(){
-    
+    let acidente;
+    let ctx = document.getElementById("myChart").getContext('2d');
+    $scope.requisicao = false;
+   
+    $scope.verifica = function(){ 
+        console.log('OKK');
+        $scope.requisicao = true;
         systemService.getUf().
         then(function successCalback(response){
             acidente = response.data;
-            criarGrafico()
-            console.log('OK',acidente)
+            $scope.requisicao = false;
+            $scope.criaGrafico()
         },
         function errorCallback(response){
             console.error('Erro ' + response);
         });
-    }
+    } 
+   
+   $scope.criaGrafico = function(){ 
     
-    /*
-    criarGrafico = function(){
-        $scope.labels = [
-            acidente[0][0],  
+    console.log('OK')
+    let labels = [
+        acidente[0][0],  
 			acidente[1][0], 
 			acidente[2][0], 
 		    acidente[3][0],  
@@ -45,10 +48,11 @@ app.controller('systemCtrl', function($scope, systemService){
             acidente[23][0],  
             acidente[24][0],  
             acidente[25][0],  
-            acidente[26][0]  
-        ];
-        $scope.data = [
-            acidente[0][1],  
+            acidente[26][0] 
+    ]
+
+    let data = [
+        acidente[0][1],  
 			acidente[1][1], 
 			acidente[2][1], 
 		    acidente[3][1],  
@@ -75,11 +79,59 @@ app.controller('systemCtrl', function($scope, systemService){
             acidente[24][1],  
             acidente[25][1],  
             acidente[26][1]
-        ];
-    }
-    */
-   criarGrafico = function(){
-    
+        
+    ]
+        let myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+              labels: labels,
+              datasets: [{
+                backgroundColor: [
+                  "#2ecc71",
+                  "#3498db",
+                  "#95a5a6",
+                  "#9b59b6",
+                  "#f1c40f",
+                  "#CD853F",
+                  "#34495e",
+                  "#FF1493",
+                  "#FF0000",
+                  "#7CFC00",
+                  "#00FFFF",
+                  "#8A2BE2",
+                  "#FFFF00",
+                  "#63B8FF",
+                  "#CD3333",
+                  "#EE7600",
+                  "#8B2252",
+                  "#76EE00",
+                  "#CD5555",
+                  "#8B0000",
+                  "#008B8B",
+                  "#8B008B",
+                  "#CD2626",
+                  "#8B4726",
+                  "#90EE90",
+                  "#FF4500",
+                  "#A020F0"
+                ],
+                data: data
+              }]
+            },
+            options: {
+                title: {
+                   display: true,
+                   fontsize: 14,
+                   text: 'Número de acidentes por UF'
+               },
+               legend: {
+                   display: true,
+                   position: 'bottom',
+       
+               },
+               responsive: true
+           }
+          });
    }
 
 });
