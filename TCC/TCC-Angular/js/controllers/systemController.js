b4a52f250ada6c19c479127f62ba6c1ed8cf8ce7
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2018-09-25 21:05:32 -0300

    Criar Graficos

--- TCC/TCC-Angular/js/controllers/systemController.js
+++ TCC/TCC-Angular/js/controllers/systemController.js
@@ -1,79 +1,26 @@
 app.controller('systemCtrl', function($scope, systemService){
     
-    let acidente;
+    let canvas = document.getElementById('myChart')
     let ctx = document.getElementById("myChart").getContext('2d');
-    $scope.requisicao = false;
-   
-    $scope.verifica = function(){ 
-        console.log('OKK');
+    let myChart = undefined // Utilizado para limpar os gráficos
+
+    limparGrafico = function(){
+        if(myChart != undefined){
+            myChart.destroy();
+        }
+        
+    }
+
+ //Criando grafico de acidentes por UF
+    $scope.gerarUF = function(){
+        console.log('UF')
+        limparGrafico()
         $scope.requisicao = true;
         systemService.getUf().
         then(function successCalback(response){
-            acidente = response.data;
             $scope.requisicao = false;
-            $scope.criaGrafico()
-        },
-        function errorCallback(response){
-            console.error('Erro ' + response);
-        });
-    } 
-   
-   $scope.criaGrafico = function(){ 
-    
-    /* Acidentes Labels */
-    let acidentesLabel = []
-    for(ac in acidente){
-        acidentesLabel.push(acidente[ac][0])
-        console.log(acidentesLabel[ac]);
-    }
 
-    /*Acidentes data*/ 
-    let acidentesData = []
-    for(ac in acidente){
-        acidentesData.push(acidente[ac][1])
-    }
-
-    console.log('OK')
-    let labels = acidentesLabel
-    let data = acidentesData
-        let myChart = new Chart(ctx, {
-            type: 'pie',
-            data: {
-              labels: labels,
-              datasets: [{
-                backgroundColor: [
-                  "#2ecc71",
-                  "#3498db",
-                  "#95a5a6",
-                  "#9b59b6",
-                  "#f1c40f",
-                  "#CD853F",
-                  "#34495e",
-                  "#FF1493",
-                  "#FF0000",
-                  "#7CFC00",
-                  "#00FFFF",
-                  "#8A2BE2",
-                  "#FFFF00",
-                  "#63B8FF",
-                  "#CD3333",
-                  "#EE7600",
-                  "#8B2252",
-                  "#76EE00",
-                  "#CD5555",
-                  "#8B0000",
-                  "#008B8B",
-                  "#8B008B",
-                  "#CD2626",
-                  "#8B4726",
-                  "#90EE90",
-                  "#FF4500",
-                  "#A020F0"
-                ],
-                data: data
-              }]
-            },
-            options: {
+            let option = {
                 title: {
                    display: true,
                    fontSize: 28,
@@ -86,8 +33,203 @@ app.controller('systemCtrl', function($scope, systemService){
                },
                responsive: true
            }
-          });
-   }
 
+            criaGrafico(response.data,'pie',option)
+        },
+        function errorCallback(response){
+            console.error('Erro ' + response);
+        });
+    } 
+   
+   //Criando grafico de acidentes por BR
+   $scope.gerarBR = function(){ 
+    console.log('BR')
+    limparGrafico()
+    $scope.requisicao = true;
+    systemService.getBR().
+    then(function successCalback(response){
+        $scope.requisicao = false;
+
+            let option = {
+                title: {
+                   display: true,
+                   fontSize: 28,
+                   text: 'Número de acidentes por BR'
+               },
+               legend: {
+                   display: false,
+                   position: 'bottom',
+       
+               },
+               responsive: true
+           }
+
+            criaGrafico(response.data,"bar",option)
+    },
+    function errorCallback(response){
+        console.error('Erro ' + response);
+    });
+} 
+
+ //Criando grafico de acidentes por sexo
+ $scope.gerarSexo = function(){ 
+    console.log('Sexo')
+    limparGrafico()
+    $scope.requisicao = true;
+    systemService.getSexo().
+    then(function successCalback(response){
+        $scope.requisicao = false;
+
+            let option = {
+                title: {
+                   display: true,
+                   fontSize: 28,
+                   text: 'Número de acidentes por Sexo'
+               },
+               legend: {
+                   display: true,
+                   position: 'bottom',
+       
+               },
+               responsive: true
+           }
+            
+            criaGrafico(response.data,"doughnut",option)
+    },
+    function errorCallback(response){
+        console.error('Erro ' + response);
+    });
+} 
+
+ //Criando grafico de acidentes por Causa
+ $scope.gerarCausa = function(){ 
+    console.log('Causa')
+    limparGrafico()
+    $scope.requisicao = true;
+    systemService.getCausa().
+    then(function successCalback(response){
+        $scope.requisicao = false;
+
+            let option = {
+                title: {
+                   display: true,
+                   fontSize: 28,
+                   text: 'Causa dos Acidentes'
+               },
+               legend: {
+                   display: true,
+                   position: "bottom",
+       
+               },
+               responsive: true
+           }
+            
+            criaGrafico(response.data,"doughnut",option)
+    },
+    function errorCallback(response){
+        console.error('Erro ' + response);
+    });
+} 
+ //Criando grafico de acidentes por Tipo
+ $scope.gerarTipo = function(){ 
+    console.log('Tipo')
+    limparGrafico()
+    $scope.requisicao = true;
+    systemService.getTipo().
+    then(function successCalback(response){
+        $scope.requisicao = false;
+
+            let option = {
+                title: {
+                   display: true,
+                   fontSize: 28,
+                   text: 'Tipos de Acidentes'
+               },
+               legend: {
+                   display: true,
+                   position: 'bottom',
+       
+               },
+               responsive: true
+           }
+            
+            criaGrafico(response.data,"pie",option)
+    },
+    function errorCallback(response){
+        console.error('Erro ' + response);
+    });
+} 
+//Criando grafico de acidentes por Dia da Semana 
+ $scope.gerarDia = function(){ 
+    console.log('dia')
+    limparGrafico()
+    $scope.requisicao = true;
+    systemService.getdia().
+    then(function successCalback(response){
+        $scope.requisicao = false;
+
+            let option = {
+                title: {
+                   display: true,
+                   fontSize: 28,
+                   text: 'Número de acidentes por Dia'
+               },
+               legend: {
+                   display: false,
+                   position: 'bottom',
+       
+               },
+               responsive: true
+           }
+
+            criaGrafico(response.data,"bar",option)
+    },
+    function errorCallback(response){
+        console.error('Erro ' + response);
+    });
+} 
+
+randomCor = function(numCor){
+    let hexadecimais = '0123456789ABCDEF';
+    let corRandom = '#';
+    let arrayCores = [];
   
-});
+    // Pega um número aleatório no array acima
+    while(numCor >= 0){
+        for (let i = 0; i < 6; i++ ) {
+            corRandom += hexadecimais[Math.floor(Math.random() * 16)];
+        }
+        arrayCores.push(corRandom)
+        corRandom = '#'
+        numCor--;
+    }
+    return arrayCores;
+}
+
+criaGrafico = function(acidente,tipoGrafico, options){
+    console.log(acidente)
+    let numArray = 0;
+    let acidentesData = []
+    let acidentesLabel = []
+    
+    for(ac in acidente){
+        acidentesLabel.push(acidente[ac][0])
+        acidentesData.push(acidente[ac][1])
+        numArray++;
+    }
+
+    console.log('OK', acidente)
+        myChart = new Chart(ctx, {
+            type: tipoGrafico,
+            data: {
+              labels: acidentesLabel,
+              datasets: [{
+                backgroundColor: randomCor(numArray),
+                data: acidentesData
+              }]
+            },
+            options: options
+          });
+   };
+
+})
